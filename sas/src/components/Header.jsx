114efd277d45dfe71cff622317ca6c37123b08bf
juliana <jg035837@outlook.com>
2025-10-25 14:31:03 +0100

    header styling

--- sas/src/components/Header.jsx
+++ sas/src/components/Header.jsx
@@ -1,7 +1,15 @@
+import sasLogo from "../images/sas-logo.png";
+import "../styles/header.css";
+import {useState} from 'react';
+
 function Header(){
+    const [isShown, setIsShown] = useState(false);
     return (
         <div className="header-container">
-            <h1>This is the header</h1>
+            <img src = {sasLogo} alt="SAS Logo" className="sas-logo"/>
+            <div className="website-title" onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)}>
+                { isShown ? <h1><b>F</b>un <b>I</b>deas <b>O</b>n <b>N</b>ight-Out <b>A</b>ctivities</h1> :  <h1>F.I.O.N.A</h1>}
+            </div>
         </div>
     )
 }

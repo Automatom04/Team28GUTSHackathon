799c524ec056fbec369bfab9d527fb3aeffd07ec
aamukta-d <aamuktamdahagam@gmail.com>
2025-10-25 15:58:49 +0100

    yay we have everything we need now

--- backend/server.js
+++ backend/server.js
@@ -27,18 +27,28 @@ app.get("/activities", async (req, res) => {
 
     let cleanLocations = []
     let cleanNames = []
+    let cleanDescs = []
+    let images = []
     $("div.col-12.col-sm-6 div.border-bottom-light-grey.small.py-1").not(".border-top-light-grey").each((index, element) => {
       cleanLocations.push($(element).text().trim());
     });
     $("div.col-12.col-sm-6 h4:not(.mb-4) a").each((index, element) => {
       cleanNames.push($(element).text().trim());
     });
+    $("div.col-12.col-sm-6 p.card-text.mt-3").each((index, element) => {
+      cleanDescs.push($(element).text().trim());
+    })
+    $("div.col-12.col-sm-6 img").each((index, element) => {
+      images.push($(element).attr("data-src").trim());
+    })
     return_object = {
       names: cleanNames,
-      locations: cleanLocations
+      locations: cleanLocations,
+      descs: cleanDescs,
+      images: images
     }
     res.json(return_object);
   } catch (error) {
-    res.status(500).json({ message: "Error accessing the URL" });
+    res.status(500).json({ message: "Error accessing the URL" + error.toString()});
   }
 });

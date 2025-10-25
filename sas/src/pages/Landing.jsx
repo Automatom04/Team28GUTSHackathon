fba350643ab78865d59637a49f71b436429bdffb
juliana <jg035837@outlook.com>
2025-10-25 18:42:13 +0100

    trying to add submit button

--- sas/src/pages/Landing.jsx
+++ sas/src/pages/Landing.jsx
@@ -1,7 +1,10 @@
 import Header from "../components/Header";
 import SelectionSection from "../sections/SelectionSection";
+import ActivitySelection from "../sections/ActivitySelection";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Button } from "@mui/material";
+
 function Landing() {
   const [data, setData] = useState([]);
 
@@ -16,15 +19,21 @@ function Landing() {
     }
   };
 
-  useEffect(() => {
+  const submitChoices = () => {
     fetchData("2025/10/23");
-  }, [])
+    <Route path="/activity-selection" element={<ActivitySelection />} />
+  };
 
   return (
     <div>
       <Header />
       <SelectionSection />
       <h1>Hi!</h1>
+      <Button 
+            size="large"
+            style={{ margin: "20px" }}
+            onClick={() => submitChoices()}>
+        Submit</Button>
     </div>
   );
 }

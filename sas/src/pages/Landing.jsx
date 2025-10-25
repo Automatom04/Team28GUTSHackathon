6351fddb0a00aa328e0bd9edcace66764d2d289a
juliana <jg035837@outlook.com>
2025-10-25 14:55:36 +0100

    finished header and landing

--- sas/src/pages/Landing.jsx
+++ sas/src/pages/Landing.jsx
@@ -1,8 +1,11 @@
-import Header from '../components/Header'
+import Header from '../components/Header';
+import "../styles/landing.css";
 function Landing(){
     return ( <div>
         <Header/>
-        <h1>Hi!</h1>
+        <div className = "landing-container">
+            <h2>Welcome to the SAS Staff Night Out Planner! Input your team's preferences below to generate your suggested night out!</h2>
+        </div>
     </div> )
 }
 

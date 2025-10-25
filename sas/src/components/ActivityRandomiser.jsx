c51df1020f3380e558684d854b690936004b67d0
juliana <jg035837@outlook.com>
2025-10-25 17:51:54 +0100

    activity randomiser

--- sas/src/components/ActivityRandomiser.jsx
+++ sas/src/components/ActivityRandomiser.jsx
@@ -1,12 +1,19 @@
 import "../styles/activity-randomiser.css";
+import { LockOpen, Lock } from "@mui/icons-material";
+import {useState} from "react";
 
 function ActivityRandomiser(){
+    const [isLocked, setIsLocked] = useState(false);
+    console.log(isLocked);
+
     return (
         <div className = "activity-randomiser-container">
             <div className="activity-box">
                 <h1>HI</h1>
             </div>
-            <button className = "randomise-button">Randomise</button>
+            <div className="randomise-button" onClick ={() => setIsLocked(!isLocked)}>
+                { isLocked ? <LockOpen/> :  <Lock/>}
+            </div>
         </div>
     )
 }
--- sas/src/index.js
+++ sas/src/index.js
@@ -4,11 +4,12 @@ import "./index.css";
 import App from "./App";
 import Landing from "./pages/Landing";
 import reportWebVitals from "./reportWebVitals";
+import ActivitySelection from "./pages/ActivitySelection";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Landing />
+    <ActivitySelection />
   </React.StrictMode>
 );
 

a78824e765811d16798060cc55bc9637d17f5fb0
aamukta-d <aamuktamdahagam@gmail.com>
2025-10-25 22:57:55 +0100

    more lol

--- sas/src/App.js
+++ sas/src/App.js
@@ -7,24 +7,6 @@ import ActivityRandomiser from "./pages/ActivitySelection";
 import ActivitySelection from "./pages/ActivitySelection";
 
 function App() {
-  const [data, setData] = useState([]);
-
-  const fetchData = async (date) => {
-    try {
-      const result = await axios(
-        `http://localhost:5000/activities?date=${date}`
-      );
-      setData(result.data);
-      console.log(result.data);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      setData([]);
-    }
-  };
-
-  useEffect(() => {
-    fetchData("2025/10/23");
-  }, []);
 
   return (
     <BrowserRouter>

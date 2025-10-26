b390ac543be412f0987b2ba36694d813bb8c2fe0
Automatom04 <tom.hossell@gmail.com>
2025-10-26 10:19:47 +0000

    FINALLY FINISHED

--- sas/src/RunPlaceData.js
+++ sas/src/RunPlaceData.js
@@ -4,12 +4,15 @@ import {SyncedPlaceData} from './SyncedPlaceData.js';
 export function RunPlaceData(activity, budget, radius, type) {
   //Get location of specified activity
   let nightOutInfo = SyncedPlaceData("Glasgow Central", 0, 0, 0);
+  console.log("what is up");
+  console.log(nightOutInfo);
   if (nightOutInfo[0]?.length == 2)
   {  
     console.log(nightOutInfo);
     console.log(nightOutInfo[0]?.activityInfo?.location?.lng());
     console.log(nightOutInfo[0]?.nearbyVenues[0]);
     console.log(nightOutInfo[0]?.nearbyVenues[1]);
+    return nightOutInfo;
   }
 }
 

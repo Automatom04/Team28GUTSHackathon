1b1cc8e7d24f814a92c179d92310eac483b51127
Automatom04 <tom.hossell@gmail.com>
2025-10-26 09:34:19 +0000

    still just so close but not quite

--- sas/src/index.js
+++ sas/src/index.js
@@ -9,7 +9,7 @@ import RunPlaceData from "./RunPlaceData";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RunPlaceData />
   </React.StrictMode>
 );
 
